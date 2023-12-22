ea0244697379ee7665e1e738ccf315364addbd80
ianwalls <ianwalls@pku.edu.cn>
2023-12-22 17:11:59 +0800

    hw08 completion

---
+++ hw08/hw08.scm
@@ -0,0 +1,21 @@
+(define (ascending? s) (cond ((or (null? s) (null? (cdr s))) #t)
+    (else (and (<= (car s) (car(cdr s))) (ascending? (cdr s))) )
+    )
+)
+
+(define (my-filter pred s) (cond ((null? s) nil)
+    ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+    (else (my-filter pred (cdr s)))
+    )
+)
+
+(define (interleave lst1 lst2) (cond ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (append (cons (car lst1) (cons (car lst2) nil)) (interleave (cdr lst1) (cdr lst2))))
+    )
+)
+
+(define (no-repeats s) (cond ((null? s) nil)
+    (else (cons (car s) (no-repeats (my-filter (lambda (a) (not (= a (car s)))) (cdr s)))))
+    )
+)
